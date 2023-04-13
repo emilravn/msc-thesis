5af9bac03ff39488afce0828d5458e4c690da06c
Islex <alexander.palsson@hotmail.com>
2023-04-13 17:05:00 +0200

    Small corrections and cleanup

--- src/reactive_robot/reactive_robot/motor.py
+++ src/reactive_robot/reactive_robot/motor.py
@@ -8,7 +8,7 @@ in4 = 5
 enA = 12
 enB = 13
 
-DEFAULT_MOTOR_SPEED = 40
+DEFAULT_MOTOR_SPEED = 30
 
 GPIO.setmode(GPIO.BCM)
 
@@ -24,33 +24,36 @@ GPIO.setup(enB,GPIO.OUT)
 GPIO.output(in3,GPIO.LOW)
 GPIO.output(in4,GPIO.LOW)
 
-pA=GPIO.PWM(enA,20)
-pB=GPIO.PWM(enB,20)
+pA=GPIO.PWM(enA,15)
+pB=GPIO.PWM(enB,15)
 pA.start(0)
 pB.start(0)
-GPIO.output(in1,GPIO.LOW)
-GPIO.output(in2,GPIO.HIGH)
-
-GPIO.output(in3,GPIO.LOW)
-GPIO.output(in4,GPIO.HIGH)
 
 def forward():
+    GPIO.output(in1,GPIO.LOW)
+    GPIO.output(in2,GPIO.HIGH)
+
+    GPIO.output(in3,GPIO.LOW)
+    GPIO.output(in4,GPIO.HIGH)
     pA.ChangeDutyCycle(DEFAULT_MOTOR_SPEED)
     pB.ChangeDutyCycle(DEFAULT_MOTOR_SPEED)
 
 def backward():
+    GPIO.output(in1,GPIO.HIGH)
+    GPIO.output(in2,GPIO.LOW)
+
+    GPIO.output(in3,GPIO.HIGH)
+    GPIO.output(in4,GPIO.LOW)
     pA.ChangeDutyCycle(DEFAULT_MOTOR_SPEED)
     pB.ChangeDutyCycle(DEFAULT_MOTOR_SPEED)
 
-
 def turn_left():
-    pA.ChangeDutyCycle(DEFAULT_MOTOR_SPEED*0.8)
-    pB.ChangeDutyCycle(DEFAULT_MOTOR_SPEED*0.5)
-
+    pA.ChangeDutyCycle(DEFAULT_MOTOR_SPEED)
+    pB.ChangeDutyCycle(DEFAULT_MOTOR_SPEED-10)
 
 def turn_right():
-    pB.ChangeDutyCycle(DEFAULT_MOTOR_SPEED*0.8)
-    pA.ChangeDutyCycle(DEFAULT_MOTOR_SPEED*0.5)
+    pA.ChangeDutyCycle(DEFAULT_MOTOR_SPEED-10)
+    pB.ChangeDutyCycle(DEFAULT_MOTOR_SPEED)
 
 def stop():
     pA.stop()
@@ -64,85 +67,3 @@ def full_stop():
     pA.stop()
     pB.stop()
     GPIO.cleanup()
-
-
-
-
-
-# print("\n")
-# print("The default speed & direction of motor is LOW & Forward.....")
-# print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
-# print("\n")    
-
-# while(1):
-
-#     x=input()
-    
-#     if x=='r':
-#         print("run")
-#         if(temp1==1):
-#          GPIO.output(in1,GPIO.HIGH)
-#          GPIO.output(in2,GPIO.LOW)
-#          GPIO.output(in3,GPIO.HIGH)
-#          GPIO.output(in4,GPIO.LOW) 
-#          print("forward")
-#          x='z'
-#         else:
-#          GPIO.output(in1,GPIO.LOW)
-#          GPIO.output(in2,GPIO.HIGH)
-#          GPIO.output(in3,GPIO.LOW)
-#          GPIO.output(in4,GPIO.HIGH)
-#          print("backward")
-#          x='z'
-
-
-#     elif x=='s':
-#         print("stop")
-#         GPIO.output(in1,GPIO.LOW)
-#         GPIO.output(in2,GPIO.LOW)
-#         GPIO.output(in3,GPIO.LOW)
-#         GPIO.output(in4,GPIO.LOW)
-#         x='z'
-
-#     elif x=='f':
-#         print("forward")
-#         GPIO.output(in1,GPIO.HIGH)
-#         GPIO.output(in2,GPIO.LOW)
-#         GPIO.output(in3,GPIO.HIGH)
-#         GPIO.output(in4,GPIO.LOW)
-#         temp1=1
-#         x='z'
-
-#     elif x=='b':
-#         print("backward")
-#         GPIO.output(in1,GPIO.LOW)
-#         GPIO.output(in2,GPIO.HIGH)
-#         temp1=0
-#         x='z'
-
-#     elif x=='l':
-#         print("low")
-#         pA.ChangeDutyCycle(25)
-#         pB.ChangeDutyCycle(25)
-#         x='z'
-
-#     elif x=='m':
-#         print("medium")
-#         pA.ChangeDutyCycle(50)
-#         pB.ChangeDutyCycle(50)
-#         x='z'
-
-#     elif x=='h':
-#         print("high")
-#         pA.ChangeDutyCycle(75)
-#         pB.ChangeDutyCycle(75)
-#         x='z'
-     
-    
-#     elif x=='e':
-#         GPIO.cleanup()
-#         break
-    
-#     else:
-#         print("<<<  wrong data  >>>")
-#         print("please enter the defined data to continue.....")
