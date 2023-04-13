f4a1010ea8ace4a7c66aac63eb76959ac0f73f9b
Emil Ravn <emilravn@outlook.com>
2023-04-13 13:50:50 +0000

    Enable copilot and grammar fixes

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# vcs import < src/ros2.repos src (uncomment if need for )
+# vcs import < src/ros2.repos src (uncomment if using external repos source code)
 sudo apt-get update
 rosdep update
 rosdep install --from-paths src --ignore-src -y
