beec6f24ba5dbf46315653e4414e7d56e4cdd5ec
henqeb <henrikbellsund@gmail.com>
2023-04-17 16:07:01 +0000

    Attempt to fix issues with encoder
    As of this commit, when trying to add detection for encoder events, it
    returns a RuntimeError: "Failed to add edge detection". Fixes online
    suggest many things, no real fix for us is found yet.

--- util/test-scripts/encoder.py
+++ util/test-scripts/encoder.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 from time import sleep
 import math
 
+
 # Motor and encoder specifications
 gear_ratio = 20.4
 wheel_diameter = 48 # mm
@@ -12,20 +13,21 @@ counts_per_rev = encoder_cpr * gear_ratio
 # INA and INB corresponds to the right motor, INC and IND to the left motor
 MOTOR_INA = 24
 MOTOR_INB = 23
+
 MOTOR_IND = 6
 MOTOR_INC = 5
+
 MOTOR_ENA = 12
 MOTOR_ENB = 13
 # Encoder pins
 L_ENCODER_A = 26
 L_ENCODER_B = 16
-R_ENCODER_A = 9
-R_ENCODER_B = 11
+R_ENCODER_A = 25
+R_ENCODER_B = 22
 
-DEFAULT_MOTOR_SPEED = 30
+DEFAULT_MOTOR_SPEED = 50
 
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setup(MOTOR_INA,GPIO.OUT)
 GPIO.setup(MOTOR_INB,GPIO.OUT)
 GPIO.setup(MOTOR_ENA,GPIO.OUT)
@@ -35,18 +37,25 @@ GPIO.output(MOTOR_INB,GPIO.LOW)
 GPIO.setup(MOTOR_IND,GPIO.OUT)
 GPIO.setup(MOTOR_INC,GPIO.OUT)
 GPIO.setup(MOTOR_ENB,GPIO.OUT)
-GPIO.output(MOTOR_IND,GPIO.LOW)
 GPIO.output(MOTOR_INC,GPIO.LOW)
+GPIO.output(MOTOR_IND,GPIO.LOW)
 
 GPIO.setup(L_ENCODER_A, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(L_ENCODER_B, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(R_ENCODER_A, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(R_ENCODER_B, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+def event_setup():
+    # Enable interrupt detection on encoder input pins
+    GPIO.add_event_detect(L_ENCODER_A, GPIO.BOTH, callback=encoder_callback_left, bouncetime=500)
+    GPIO.add_event_detect(L_ENCODER_B, GPIO.BOTH, callback=encoder_callback_left, bouncetime=500)
+    GPIO.add_event_detect(R_ENCODER_A, GPIO.BOTH, callback=encoder_callback_right, bouncetime=500)
+    GPIO.add_event_detect(R_ENCODER_B, GPIO.BOTH, callback=encoder_callback_right, bouncetime=500) # TODO: research bouncetime
+
 pA=GPIO.PWM(MOTOR_ENA,20)
 pB=GPIO.PWM(MOTOR_ENB,20)
-pA.start(0)
-pB.start(0)
+pA.start(100)
+pB.start(100)
 
 # Set initial encoder counts
 left_count = 0
@@ -54,30 +63,45 @@ right_count = 0
 left_distance = 0
 right_distance = 0
 
+
 # Define interrupt handlers for encoder signals
 def encoder_callback_left(channel):
     global left_count, left_distance
+    sleep(0.1)
+    GPIO.remove_event_detect(L_ENCODER_A)
+    GPIO.remove_event_detect(L_ENCODER_B)
+
     if GPIO.input(L_ENCODER_A) == GPIO.input(L_ENCODER_B):
         left_count += 1
+        print("Incremented left_count") # TODO: remove
     else:
         left_count -= 1
+        print("decremented left_count") # TODO: remove
     left_distance = distance_traveled(left_count)
+
+    print("hello big boy")
+    sleep(0.1)
+    GPIO.add_event_detect(L_ENCODER_A, GPIO.BOTH, callback=encoder_callback_left, bouncetime=500)
+    GPIO.add_event_detect(L_ENCODER_B, GPIO.BOTH, callback=encoder_callback_left, bouncetime=500)
 
 
 def encoder_callback_right(channel):
     global right_count, right_distance
+    sleep(0.1)
+    GPIO.remove_event_detect(R_ENCODER_A)
+    GPIO.remove_event_detect(R_ENCODER_B)
+
     if GPIO.input(R_ENCODER_A) == GPIO.input(R_ENCODER_B):
         right_count += 1
+        print("Incremented right_count") # TODO: remove
     else:
         right_count -= 1
+        print("decremented right_count") # TODO: remove
     right_distance = distance_traveled(right_count)
 
-
-# Enable interrupt detection on encoder input pins
-GPIO.add_event_detect(L_ENCODER_A, GPIO.BOTH, callback=encoder_callback_left)
-GPIO.add_event_detect(L_ENCODER_B, GPIO.BOTH, callback=encoder_callback_left)
-GPIO.add_event_detect(R_ENCODER_A, GPIO.BOTH, callback=encoder_callback_right)
-GPIO.add_event_detect(R_ENCODER_B, GPIO.BOTH, callback=encoder_callback_right)
+    print("hello big girl")
+    sleep(0.1)
+    event_setup()
 
 
 def forward():
@@ -131,8 +155,23 @@ def distance_traveled(counts):
     return distance
 
 
-def main():
-    forward()
+def main(): 
+    global left_count, left_distance, right_count, right_distance
+
+    while True:
+        left_dist_cm = left_distance*0.1
+        right_dist_cm = right_distance*0.1
+        total_distance_cm = (left_dist_cm+right_dist_cm)/2
+        event_setup() 
+        forward()
+        # print(f"Left motor count = {left_count}, distance = {left_dist_cm}")
+        # print(f"Right motor count = {right_count}, distance = {right_dist_cm}")
+        # print(f"Total distance covered = {total_distance_cm}")
+
+        if total_distance_cm == 50:
+            full_stop()
+            print(f"Full stop, total distance = {total_distance_cm}")
+            break
 
 if __name__ == "__main__":
     try:
@@ -140,4 +179,5 @@ if __name__ == "__main__":
  
     except KeyboardInterrupt:
         print("Measurement stopped by User")
+    finally:
         GPIO.cleanup()
