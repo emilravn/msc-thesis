a3e8bd4f3ed62a8b2f66f82afc2c030b662dab6f
henqeb <henrikbellsund@gmail.com>
2023-04-18 14:32:27 +0000

    Remove redundant comments, variables and imports

--- util/test-scripts/encoder.py
+++ util/test-scripts/encoder.py
@@ -1,27 +1,24 @@
 import math
-from gpiozero import RotaryEncoder, Robot, PWMOutputDevice, Motor
+from gpiozero import RotaryEncoder, Robot
 import RPi.GPIO as GPIO          
 from time import sleep
 
 # L298N motor driver pins
-# INA and INB corresponds to the right motor, INC and IND to the left motor
-
 # RIGHT
 MOTOR_INA = 24
 MOTOR_INB = 23
 MOTOR_ENA = 12
+R_ENCODER_A = 25
+R_ENCODER_B = 22
 # LEFT
 MOTOR_IND = 6
 MOTOR_INC = 5
 MOTOR_ENB = 13
-
-# Encoder pins
 L_ENCODER_A = 26
 L_ENCODER_B = 16
-R_ENCODER_A = 25
-R_ENCODER_B = 22
 
-DEFAULT_MOTOR_SPEED = 60
+DEFAULT_MOTOR_SPEED = 40
+
 
 robot = Robot(left=(MOTOR_INC, MOTOR_IND, MOTOR_ENA), right=(MOTOR_INA, MOTOR_INB, MOTOR_ENB), pwm=False)
 GPIO.setmode(GPIO.BCM)
@@ -31,11 +28,7 @@ pwm_right = GPIO.PWM(MOTOR_ENA, 20)
 pwm_left = GPIO.PWM(MOTOR_ENB, 20)
 pwm_right.start(DEFAULT_MOTOR_SPEED)
 pwm_left.start(DEFAULT_MOTOR_SPEED)
-# right_motor = Motor(MOTOR_INA, MOTOR_INB, pwm=False)
-# left_motor = Motor(MOTOR_INC, MOTOR_IND, pwm=False)
 
-
-# Create an instance of the RotaryEncoder class
 encoder_left = RotaryEncoder(
     L_ENCODER_A, 
     L_ENCODER_B, 
@@ -49,10 +42,9 @@ encoder_right = RotaryEncoder(
     wrap=False)
 
 # Define the distance to travel (in cm)
-distance_to_travel = 400
+distance_to_travel = 150
 distance_covered_left = 0
 distance_covered_right = 0
-distance_travelled = 0
 
 # Encoder/motor specs 
 gear_ratio = 20.4
@@ -60,24 +52,26 @@ wheel_diameter = 48 # millimeter
 encoder_cpr = 48
 counts_per_rev = encoder_cpr * gear_ratio
 
+
 def distance_travelled(counts):
     revs = counts / counts_per_rev
     distance = revs * math.pi * wheel_diameter
     return distance
 
-# Define a callback function to be called each time the encoder position changes
+
 def on_rotate_left():
     global distance_covered_left
     distance_covered_left = distance_travelled(encoder_left.steps)
+
 
 def on_rotate_right():
     global distance_covered_right
     distance_covered_right = distance_travelled(encoder_right.steps)
 
+
 if __name__ == "__main__":
     try:
         while True:
-            # Attach the callback function to the RotaryEncoder instance
             encoder_left.when_rotated = on_rotate_left()
             encoder_right.when_rotated = on_rotate_right()
             robot.forward()
