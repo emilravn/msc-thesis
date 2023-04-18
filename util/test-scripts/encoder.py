30b27937da19320e5fe2a82d3d0266862877d077
henqeb <90144946+henqeb@users.noreply.github.com>
2023-04-18 16:47:33 +0200

    Encoder (#18)
    * Encoder code working.
    
    * Added gpiozero req
    
    * Enable encoder and motor coordination.
    
    Encoders are set up with "traditional" GPIO setup, motors are set up
    using gpiozero.
    
    * Remove redundant comments, variables and imports
    
    * Add camera node (#17)
    
    * Store dev and prod requirements separately
    
    * Add Flake8 rules
    
    * Remove trash
    
    * Fix small correction
    
    * Encoder code working.
    
    * Added gpiozero req
    
    * Add camera node
    
    * Remove examples folder
    
    * Rename back to original for dev
    
    * Correction
    
    ---------
    
    Co-authored-by: Islex <alexander.palsson@hotmail.com>
    
    ---------
    
    Co-authored-by: Islex <alexander.palsson@hotmail.com>
    Co-authored-by: Emil Ravn <46424499+emilravn@users.noreply.github.com>

--- util/test-scripts/encoder.py
+++ util/test-scripts/encoder.py
@@ -4,26 +4,31 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 # L298N motor driver pins
-# INA and INB corresponds to the right motor, INC and IND to the left motor
-
 # RIGHT
 MOTOR_INA = 24
 MOTOR_INB = 23
+MOTOR_ENA = 12
+R_ENCODER_A = 25
+R_ENCODER_B = 22
 # LEFT
 MOTOR_IND = 6
 MOTOR_INC = 5
-
-MOTOR_ENA = 12
 MOTOR_ENB = 13
-# Encoder pins
 L_ENCODER_A = 26
 L_ENCODER_B = 16
-R_ENCODER_A = 25
-R_ENCODER_B = 22
 
-robot = Robot(left=(MOTOR_INC, MOTOR_IND, MOTOR_ENA), right=(MOTOR_INA, MOTOR_INB, MOTOR_ENB))
+DEFAULT_MOTOR_SPEED = 40
 
-# Create an instance of the RotaryEncoder class
+
+robot = Robot(left=(MOTOR_INC, MOTOR_IND, MOTOR_ENA), right=(MOTOR_INA, MOTOR_INB, MOTOR_ENB), pwm=False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(MOTOR_ENA,GPIO.OUT)
+GPIO.setup(MOTOR_ENB,GPIO.OUT)
+pwm_right = GPIO.PWM(MOTOR_ENA, 20)
+pwm_left = GPIO.PWM(MOTOR_ENB, 20)
+pwm_right.start(DEFAULT_MOTOR_SPEED)
+pwm_left.start(DEFAULT_MOTOR_SPEED)
+
 encoder_left = RotaryEncoder(
     L_ENCODER_A, 
     L_ENCODER_B, 
@@ -36,19 +41,17 @@ encoder_right = RotaryEncoder(
     max_steps=0, 
     wrap=False)
 
-DEFAULT_MOTOR_SPEED = 30
-
 # Define the distance to travel (in cm)
-distance_to_travel = 50
+distance_to_travel = 150
 distance_covered_left = 0
 distance_covered_right = 0
-distance_travelled = 0
 
-# Encoder/motor specs
+# Encoder/motor specs 
 gear_ratio = 20.4
-wheel_diameter = 48
+wheel_diameter = 48 # millimeter
 encoder_cpr = 48
 counts_per_rev = encoder_cpr * gear_ratio
+
 
 def distance_travelled(counts):
     revs = counts / counts_per_rev
@@ -60,22 +63,28 @@ def on_rotate_left():
     global distance_covered_left
     distance_covered_left = distance_travelled(encoder_left.steps)
 
+def on_rotate_left():
+    global distance_covered_left
+    distance_covered_left = distance_travelled(encoder_left.steps)
+
+
 def on_rotate_right():
     global distance_covered_right
     distance_covered_right = distance_travelled(encoder_right.steps)
 
+
 if __name__ == "__main__":
     try:
         while True:
-            # Attach the callback function to the RotaryEncoder instance
             encoder_left.when_rotated = on_rotate_left()
             encoder_right.when_rotated = on_rotate_right()
             robot.forward()
             total_distance_covered = (distance_covered_left + distance_covered_right) / 2
             print(f"total_distance_covered: {abs(total_distance_covered)}")
 
-            if abs(total_distance_covered) >= distance_to_travel:
+            if abs(total_distance_covered) > distance_to_travel:
                 robot.stop()
+                break
 
     except KeyboardInterrupt:
         print("Stopped")
