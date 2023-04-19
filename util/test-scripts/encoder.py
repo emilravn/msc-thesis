a241a84066ebd8235863be47a49b5aac2054f46f
henqeb <henrikbellsund@gmail.com>
2023-04-19 08:36:06 +0000

    Format encoder script

--- util/test-scripts/encoder.py
+++ util/test-scripts/encoder.py
@@ -1,7 +1,6 @@
 import math
 from gpiozero import RotaryEncoder, Robot
-import RPi.GPIO as GPIO          
-from time import sleep
+import RPi.GPIO as GPIO
 
 # L298N motor driver pins
 # RIGHT
@@ -20,25 +19,26 @@ L_ENCODER_B = 16
 DEFAULT_MOTOR_SPEED = 40
 
 
-robot = Robot(left=(MOTOR_INC, MOTOR_IND, MOTOR_ENA), right=(MOTOR_INA, MOTOR_INB, MOTOR_ENB), pwm=False)
+robot = Robot(left=(MOTOR_INC, MOTOR_IND, MOTOR_ENA),
+              right=(MOTOR_INA, MOTOR_INB, MOTOR_ENB), pwm=False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(MOTOR_ENA,GPIO.OUT)
-GPIO.setup(MOTOR_ENB,GPIO.OUT)
+GPIO.setup(MOTOR_ENA, GPIO.OUT)
+GPIO.setup(MOTOR_ENB, GPIO.OUT)
 pwm_right = GPIO.PWM(MOTOR_ENA, 20)
 pwm_left = GPIO.PWM(MOTOR_ENB, 20)
 pwm_right.start(DEFAULT_MOTOR_SPEED)
 pwm_left.start(DEFAULT_MOTOR_SPEED)
 
 encoder_left = RotaryEncoder(
-    L_ENCODER_A, 
-    L_ENCODER_B, 
-    max_steps=0, 
+    L_ENCODER_A,
+    L_ENCODER_B,
+    max_steps=0,
     wrap=False)
 
 encoder_right = RotaryEncoder(
-    R_ENCODER_A, 
-    R_ENCODER_B, 
-    max_steps=0, 
+    R_ENCODER_A,
+    R_ENCODER_B,
+    max_steps=0,
     wrap=False)
 
 # Define the distance to travel (in cm)
@@ -46,9 +46,9 @@ distance_to_travel = 150
 distance_covered_left = 0
 distance_covered_right = 0
 
-# Encoder/motor specs 
+# Encoder/motor specs
 gear_ratio = 20.4
-wheel_diameter = 48 # millimeter
+wheel_diameter = 48  # millimeter
 encoder_cpr = 48
 counts_per_rev = encoder_cpr * gear_ratio
 
@@ -59,9 +59,7 @@ def distance_travelled(counts):
     return distance
 
 # Define a callback function to be called each time the encoder position changes
-def on_rotate_left():
-    global distance_covered_left
-    distance_covered_left = distance_travelled(encoder_left.steps)
+
 
 def on_rotate_left():
     global distance_covered_left
