0b370a01c8d3296cf08b92eb5097de9f23502664
henqeb <henrikbellsund@gmail.com>
2023-04-19 10:07:49 +0000

    Change specifications of wheels to match the whole belt
    Encoder/motor specs was currently defined to match a single wheel (the
    one attached to the motor), and is now defined to match the diameter of
    the whole belt (because of physics :) )

--- util/test-scripts/encoder.py
+++ util/test-scripts/encoder.py
@@ -16,7 +16,7 @@ MOTOR_ENB = 13
 L_ENCODER_A = 26
 L_ENCODER_B = 16
 
-DEFAULT_MOTOR_SPEED = 40
+DEFAULT_MOTOR_SPEED = 60
 
 
 robot = Robot(left=(MOTOR_INC, MOTOR_IND, MOTOR_ENA),
@@ -42,20 +42,20 @@ encoder_right = RotaryEncoder(
     wrap=False)
 
 # Define the distance to travel (in cm)
-distance_to_travel = 150
+distance_to_travel = 25
 distance_covered_left = 0
 distance_covered_right = 0
 
 # Encoder/motor specs
 gear_ratio = 20.4
-wheel_diameter = 48  # millimeter
+belt_diameter = 105  # millimeter
 encoder_cpr = 48
 counts_per_rev = encoder_cpr * gear_ratio
 
 
 def distance_travelled(counts):
     revs = counts / counts_per_rev
-    distance = revs * math.pi * wheel_diameter
+    distance = revs * math.pi * belt_diameter
     return distance
 
 # Define a callback function to be called each time the encoder position changes
