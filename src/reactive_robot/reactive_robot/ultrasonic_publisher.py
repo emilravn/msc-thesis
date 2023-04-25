adaa90237da7dd57fbcce0daef03d6f94cf271f6
Emil Ravn <emilravn@outlook.com>
2023-04-25 14:15:34 +0000

    Add all sensors to ultrasonic publisher, currently deadlocks

--- src/reactive_robot/reactive_robot/ultrasonic_publisher.py
+++ src/reactive_robot/reactive_robot/ultrasonic_publisher.py
@@ -1,28 +1,32 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Range
+from functools import partial
 
 from .ultrasonic import Sonar
 
-# Ultrasonic sensor setup
-GPIO_TRIGGER = 17
-GPIO_ECHO = 27
-us_sensor = Sonar(GPIO_TRIGGER, GPIO_ECHO)
+middle = Sonar('back', 27, 17)
+back = Sonar('middle', 7, 8)
+front = Sonar('front', 15, 14)
+
+SPIN_QUEUE = []
+PERIOD = 0.01
 
 
 class UltrasonicPublisher(Node):  # 'MinimalPublisher' is a subclass (inherits) of 'Node'
 
-    def __init__(self):
-        super().__init__('ultrasonic_publisher')
+    def __init__(self, sensor: Sonar, topic):
+        super().__init__(sensor.location)
         # 3rd parameter, 'qos_profile' is "queue size"
 
-        self.ultrasonic_publisher_ = self.create_publisher(Range, 'ultrasonic/distance', 10)
-        ultrasonic_timer_period = 0.2  # seconds
-        self.ultrasonic_timer = self.create_timer(
-            ultrasonic_timer_period, self.ultrasonic_callback)
+        # seconds
+        timer_period = 0.2
+        callback_function = partial(self.callback, sensor)
+        self.ultrasonic_publisher_ = self.create_publisher(Range, topic, 10)
+        self.ultrasonic_timer = self.create_timer(timer_period, callback_function)
 
-    def ultrasonic_callback(self):
-        us_distance = us_sensor.get_distance()
+    def callback(self, sensor: Sonar):
+        us_distance = sensor.get_distance()
         msg = Range()
         msg.header.stamp = self.get_clock().now().to_msg()
         msg.header.frame_id = "/sonar_link"
@@ -31,30 +35,35 @@ class UltrasonicPublisher(Node):  # 'MinimalPublisher' is a subclass (inherits) 
         msg.min_range = 0.0
         msg.max_range = 10.0
         msg.range = us_distance
-        self.get_logger().info('Ultrasonic publishing: "%f"' % msg.range)
+        self.get_logger().info(f'{sensor} sensor publishing: "%f"' % msg.range)
         self.ultrasonic_publisher_.publish(msg)
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    ultrasonic_publisher = UltrasonicPublisher()
+    back_sensor_node = UltrasonicPublisher(back, 'ultrasonic/back/distance')
+    middle_sensor_node = UltrasonicPublisher(middle, 'ultrasonic/middle/distance')
+    front_sensor_node = UltrasonicPublisher(front, 'ultrasonic/front/distance')
 
-    rclpy.spin(ultrasonic_publisher)
+    SPIN_QUEUE.append(back_sensor_node)
+    SPIN_QUEUE.append(middle_sensor_node)
+    SPIN_QUEUE.append(front_sensor_node)
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
+    while rclpy.ok():
+        try:
+            for node in SPIN_QUEUE:
+                rclpy.spin_once(node, timeout_sec=(PERIOD / len(SPIN_QUEUE)))
+        except Exception as e:
+            print(f"something went wrong in the ROS Loop: {e}")
+
+    # Destroy the nodes (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    us_sensor.cleanup_pins()
-    ultrasonic_publisher.destroy_node()
+    back_sensor_node.destroy_node()
+    middle_sensor_node.destroy_node()
+    front_sensor_node.destroy_node()
     rclpy.shutdown()
 
 
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print("ultrasonic_publisher stopped by User")
-        us_sensor.cleanup_pins()
-    finally:
-        us_sensor.cleanup_pins()
+    main()
