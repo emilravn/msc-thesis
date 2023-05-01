c31342f95e56c6218b490fa9395f2bff09c15e43
Islex <alexander.palsson@hotmail.com>
2023-05-01 14:40:51 +0000

    Tried making motors "warm-up" and works in this script

--- util/motor.py
+++ util/motor.py
@@ -16,22 +16,19 @@ MOTOR_ENB = 13
 class Motor():
 
     def __init__(self, inA: int, inB: int, inC: int, inD: int,
-                 enA: int, enB: int,
-                 initial_speed: float = 80.0):
+                 enA: int, enB: int):
 
-        self.motors = Robot(left=(inC, inD, enB),
-                            right=(inA, inB, enA),
+        self.motors = Robot(left=(inC, inD),
+                            right=(inA, inB),
                             pwm=False)
         # GPIO setup
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(enA, GPIO.OUT)
         GPIO.setup(enB, GPIO.OUT)
-        self.pwm_left = GPIO.PWM(enB, 20)
-        self.pwm_right = GPIO.PWM(enA, 20)
-        self.pwm_left.start(100.0)
-        self.pwm_right.start(100.0)
-        sleep(0.2)  # give motors time to start
-        self.set_speed(initial_speed, initial_speed)
+        self.pwm_left = GPIO.PWM(enB, 5)
+        self.pwm_right = GPIO.PWM(enA, 5)
+        self.pwm_left.start(100)
+        self.pwm_right.start(100)
 
     def set_speed(self, left_speed: float, right_speed: float):
         self.pwm_left.ChangeDutyCycle(left_speed)
@@ -46,10 +43,15 @@ if __name__ == "__main__":
         robot = Motor(MOTOR_INA, MOTOR_INB, MOTOR_INC, MOTOR_IND,
                       MOTOR_ENA, MOTOR_ENB)
         
-        robot.motors.left()
-        sleep(1.5)
+        robot.set_speed(40, 40)
+        robot.motors.forward()
+        sleep(0.3)
+        robot.set_speed(100, 100)
+        robot.motors.forward()
+        sleep(5)
 
     except KeyboardInterrupt:
         print("Stopped by user")
+        GPIO.cleanup()
     finally:
         GPIO.cleanup()
