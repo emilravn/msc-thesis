4eaecb7c669b44bd40be5c315682a278e351b9a4
Islex <alexander.palsson@hotmail.com>
2023-05-01 14:43:04 +0000

    Converting motor controller to adjust frequency.

--- src/reactive_robot/reactive_robot/motor_controller.py
+++ src/reactive_robot/reactive_robot/motor_controller.py
@@ -1,32 +1,40 @@
 from gpiozero import Robot
+import RPi.GPIO as GPIO
 from time import sleep
 
 
 class MotorController():
-    def __init__(self, inA: int = 24, inB: int = 23, inC: int = 5, inD: int = 6,
+    def __init__(self,
+                 inA: int = 24, inB: int = 23,
+                 inC: int = 5, inD: int = 6,
                  enA: int = 12, enB: int = 13):
 
-        self.motors = Robot(left=(inC, inD, enB), right=(inA, inB, enA), pwm=True)
+        self.motors = Robot(left=(inC, inD), right=(inA, inB), pwm=False)
 
-    def set_speed(self, left_speed: float = None, right_speed: float = None):
-        if right_speed:
-            self.motors.forward(curve_left=right_speed)
-        if left_speed:
-            self.motors.forward(curve_right=left_speed)
-        else:
-            self.motors.forward()
+        # GPIO setup
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(enA, GPIO.OUT)
+        GPIO.setup(enB, GPIO.OUT)
+        self.pwm_left = GPIO.PWM(enB, 20)
+        self.pwm_right = GPIO.PWM(enA, 20)
+        self.pwm_left.start(100)
+        self.pwm_right.start(100)
 
-    def drive_forward(self, speed: float):
-        self.motors.forward(speed)
+    def set_speed(self, left_speed: float, right_speed: float):
+        self.pwm_left.ChangeDutyCycle(left_speed)
+        self.pwm_right.ChangeDutyCycle(right_speed)
 
-    def drive_backward(self, speed: float):
-        self.motors.backward(speed)
+    def drive_forward(self):
+        self.motors.forward()
+
+    def drive_backward(self):
+        self.motors.backward()
 
     def turn_left(self):
-        self.motors.right()
+        self.motors.left()
 
     def turn_right(self):
-        self.motors.left()
+        self.motors.right()
 
     def curve_left(self, speed: float):
         self.motors.forward(curve_left=speed)
