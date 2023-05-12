96076eb658571084ab8a4baaccfdb34219c92c53
Emil Ravn <emilravn@outlook.com>
2023-05-12 16:49:37 +0000

    Add docstrings and additional methods

--- src/reactive_robot/reactive_robot/motor_controller.py
+++ src/reactive_robot/reactive_robot/motor_controller.py
@@ -1,5 +1,4 @@
 from gpiozero import Robot, PWMOutputDevice
-from time import sleep
 
 
 class MotorController:
@@ -13,12 +12,27 @@ class MotorController:
         enB: int = 13,
     ):
         self.motors = Robot(left=(inC, inD), right=(inA, inB), pwm=True)
-
         self.pwmA = PWMOutputDevice(enA, active_high=True, frequency=30, initial_value=0.75)
         self.pwmB = PWMOutputDevice(enB, active_high=True, frequency=30, initial_value=0.75)
 
     def set_speed(self, left_speed: float, right_speed: float):
+        """
+        Set speed for left and right motor betwen -1 and 1 where (-1, -1) full speed backwards,
+        (0, 0 is stop) and (-1, -1) is full speed forwards.
+        """
         self.motors.value = (left_speed, right_speed)
+
+    def get_speed(self):
+        """
+        Return current speed for both motors as a tuple.
+        """
+        return self.motors.value
+
+    def stop(self):
+        """
+        Set speed to 0 and thus stop the motor.
+        """
+        self.motors.stop()
 
     def drive_forward(self):
         self.motors.forward()
@@ -37,37 +51,3 @@ class MotorController:
 
     def curve_right(self, speed: float):
         self.motors.forward(curve_right=speed)
-
-
-if __name__ == "__main__":
-    robot = MotorController()
-    robot.set_speed(0.1, 0.1)
-    print("0.1")
-    sleep(3)
-    robot.set_speed(0.2, 0.2)
-    print("0.2")
-    sleep(3)
-    robot.set_speed(0.3, 0.3)
-    print("0.3")
-    sleep(3)
-    robot.set_speed(0.4, 0.4)
-    print("0.4")
-    sleep(3)
-    robot.set_speed(0.5, 0.5)
-    print("0.5")
-    sleep(3)
-    robot.set_speed(0.6, 0.6)
-    print("0.6")
-    sleep(3)
-    robot.set_speed(0.7, 0.7)
-    print("0.7")
-    sleep(3)
-    robot.set_speed(0.8, 0.8)
-    print("0.8")
-    sleep(3)
-    robot.set_speed(0.9, 0.9)
-    print("0.9")
-    sleep(3)
-    robot.set_speed(1, 1)
-    print("1")
-    sleep(3)
