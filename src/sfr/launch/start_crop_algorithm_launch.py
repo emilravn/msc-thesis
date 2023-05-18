efca234f7903f6d3522c0f99579c1f557dbb51c7
Emil Ravn <emilravn@outlook.com>
2023-05-18 15:20:18 +0000

    Add launch file to the entire algorithm

---
+++ src/sfr/launch/start_crop_algorithm_launch.py
@@ -0,0 +1,27 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='sfr',
+            executable='encoder_publisher_node',
+            name='encoder_publisher_node'
+        ),
+        Node(
+            package='sfr',
+            executable='ultrasonic_publisher_node',
+            name='ultrasonic_publisher_node'
+        ),
+        Node(
+            package='sfr',
+            executable='scd30_publisher_node',
+            name='scd30_publisher_node'
+        ),
+        Node(
+            package='sfr',
+            executable='crop_follower_node',
+            name='crop_follower_node'
+        )
+    ])
