d009a41ed32436d5e979be5c17ec3912f2d423a9
henqeb <henrikbellsund@gmail.com>
2023-05-19 14:10:47 +0000

    Initial setup for pid experiments plotting

--- docs/experiment-plots/straight-line-plot.py
+++ docs/experiment-plots/straight-line-plot.py
@@ -28,7 +28,107 @@ def summary_boxplot(all_min_distances, output_destination_folder, fig_width=10, 
     plt.savefig(f"{output_destination_folder}/summary_box_plot.png")
 
 
-if __name__ == "__main__":
+def plot_pid_experiments():
+    cases = [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+    ]
+    plot_titles = []
+    output_folder = "pid-experiments/kd"
+
+    # Store all data for summary plot
+    summary_data = []
+    all_min_distances = []
+
+    for case in cases:
+        plt.figure()
+
+        bag_file = "../experiments/1-straight-line/{}/{}_0.db3".format(case, case)
+
+        parser = BagFileParser(bag_file)
+
+        encoder = parser.get_messages("/total_encoder/distance")
+        front = parser.get_messages("/ultrasonic/front/distance")
+        middle = parser.get_messages("/ultrasonic/middle/distance")
+        back = parser.get_messages("/ultrasonic/back/distance")
+
+        encoder_timestamps = [encoder[i][0] for i in range(len(encoder))]
+        encoder_distances = [encoder[i][1].data for i in range(len(encoder))]
+
+        front_timestamps = [front[i][0] for i in range(len(front))]
+        front_distances = [front[i][1].range for i in range(len(front))]
+
+        middle_timestamps = [middle[i][0] for i in range(len(middle))]
+        middle_distances = [middle[i][1].range for i in range(len(middle))]
+
+        back_timestamps = [back[i][0] for i in range(len(back))]
+        back_distances = [back[i][1].range for i in range(len(back))]
+
+        front_distances_interp = np.interp(encoder_timestamps, front_timestamps, front_distances)
+        middle_distances_interp = np.interp(
+            encoder_timestamps, middle_timestamps, middle_distances
+        )
+        back_distances_interp = np.interp(encoder_timestamps, back_timestamps, back_distances)
+
+        encoder_filtered = []
+        min_distances_filtered = []
+        for i in range(len(encoder)):
+            encoder_value = encoder_distances[i]
+            front_value = front_distances_interp[i]
+            middle_value = middle_distances_interp[i]
+            back_value = back_distances_interp[i]
+
+            if encoder_value != 0:
+                encoder_filtered.append(encoder_value)
+                min_distance = min(front_value, middle_value, back_value)
+                min_distances_filtered.append(min_distance)
+
+        plt.plot(encoder_filtered, min_distances_filtered)
+        plt.xlabel("Distance driven (cm)")
+        plt.ylabel("Distance to wall (cm)")
+        plt.title(f"Straight line experiment for {case}")
+
+        plt.savefig(f"{output_folder}/{case}_plot.png")
+
+        plt.close()
+
+        # Store data for summary plot
+        summary_data.append((encoder_filtered, min_distances_filtered))
+        all_min_distances.extend(min_distances_filtered)
+
+    # Summary plot
+    plt.figure(figsize=(14, 10))
+
+    for i, (encoder_filtered, min_distances_filtered) in enumerate(summary_data):
+        plt.plot(encoder_filtered, min_distances_filtered, label=cases[i])
+
+    # Add acceptance range lines
+    plt.axhline(
+        y=18, color="black", linestyle="--", label="Lower/Upper acceptance limit (18/22 cm)"
+    )
+    plt.axhline(y=22, color="black", linestyle="--")
+
+    plt.xlabel("Distance driven (cm)")
+    plt.ylabel("Distance to wall (cm)")
+    plt.title("Straight line experiment")
+    plt.legend(loc="lower right")  # Legend moved to outside of plot
+
+    plt.savefig(f"{output_folder}/summary_plot.png")
+
+    # Individual cases box plot
+    individual_summary_boxplot(summary_data, output_folder)
+    # Summarizing box plot
+    summary_boxplot(all_min_distances, output_folder)
+
+
+def main():
     cases = [
         "case1",
         "case2",
@@ -126,3 +226,7 @@ if __name__ == "__main__":
     individual_summary_boxplot(summary_data, output_folder)
     # Summarizing box plot
     summary_boxplot(all_min_distances, output_folder)
+
+
+if __name__ == "__main__":
+    main()
