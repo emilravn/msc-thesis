7baf54a41db67c155c420d5013bf26c53f14f423
Emil Ravn <emilravn@outlook.com>
2023-05-24 13:29:44 +0000

    Say goodbye to util

---
+++ classifier/inferencing_one_image.py
@@ -0,0 +1,69 @@
+from tflite_runtime.interpreter import Interpreter
+from PIL import Image
+import numpy as np
+import time
+import os
+
+
+data_folder = "./images/plantvillage/"
+
+model_path = "./model/efficient_test1_data10.tflite"
+label_path = "./model/labels.txt"
+
+interpreter = Interpreter(model_path)
+print("Model Loaded Successfully.")
+
+interpreter.allocate_tensors()
+# Get input and output tensors.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+_, height, width, _ = interpreter.get_input_details()[0]["shape"]
+print("Image Shape (", width, ",", height, ")")
+
+# Test the model on random input data.
+
+# Find the file name of a random image in the data_folder
+file_name = np.random.choice(os.listdir(data_folder))
+
+image = Image.open(data_folder + "tomato_early_blight.jpg").convert("RGB").resize((width, height))
+image = np.array(image)
+processed_image = np.expand_dims(image, axis=0).astype(np.float32)
+
+interpreter.set_tensor(input_details[0]["index"], processed_image)
+
+t1 = time.time()
+interpreter.invoke()
+t2 = time.time()
+time_taken = (t2 - t1) * 1000  # milliseconds
+print("time taken for Inference: ", str(time_taken), "ms")
+
+# Obtain results
+prediction = interpreter.get_tensor(output_details[0]["index"])[0]
+print(prediction)
+prediction = np.argmax(prediction)
+print(prediction)
+
+
+def compare_output(output_value, label_path):
+    # Read the contents of the text file
+    with open(label_path, "r") as file:
+        lines = file.readlines()
+
+    # Create a dictionary to store the text file contents
+    labels = {}
+    for line in lines:
+        parts = line.strip().split(" ")
+        label_key = int(parts[0])
+        disease = parts[1]
+        labels[label_key] = disease
+
+    # Compare the output value with the text file entry
+    if output_value in labels:
+        print(f"Output: {output_value}")
+        print(f"Text: {labels[output_value]}")
+    else:
+        print("Output value not found in the text file.")
+
+
+compare_output(prediction, label_path)
