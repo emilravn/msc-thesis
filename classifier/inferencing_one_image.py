b9d71d5f6f9882ef9cf462f08fa82f2a3c2ae270
Emil Ravn <emilravn@outlook.com>
2023-05-24 15:22:24 +0000

    Changes

--- classifier/inferencing_one_image.py
+++ classifier/inferencing_one_image.py
@@ -44,9 +44,7 @@ print("time taken for Inference: ", str(time_taken), "ms")
 
 # Obtain results
 prediction = interpreter.get_tensor(output_details[0]["index"])[0]
-print(prediction)
 prediction = np.argmax(prediction)
-print(prediction)
 
 
 def compare_output(output_value, label_path):
@@ -59,13 +57,13 @@ def compare_output(output_value, label_path):
     for line in lines:
         parts = line.strip().split(" ")
         label_key = int(parts[0])
-        disease = parts[1]
+        disease = " ".join(parts[1:])
         labels[label_key] = disease
 
     # Compare the output value with the text file entry
     if output_value in labels:
-        print(f"Output: {output_value}")
-        print(f"Text: {labels[output_value]}")
+        print(f"Image: {tomato_class_folder}{random_image}")
+        print(f"Identified by model as: {labels[output_value]}")
     else:
         print("Output value not found in the text file.")
 
