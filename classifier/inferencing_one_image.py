39a8c1ba465994c36ba5a22b28e7d945f3c93fb3
Emil Ravn <emilravn@outlook.com>
2023-05-24 16:04:13 +0000

    Add statistics script

--- classifier/inferencing_one_image.py
+++ classifier/inferencing_one_image.py
@@ -5,10 +5,8 @@ import time
 import os
 import random
 
-# Find a random image in the dataset
+# Image dataset
 images_folder = "./images/plantvillage/"
-tomato_class_folder = str(np.random.choice(os.listdir(images_folder)) + "/")
-full_path = os.path.join(images_folder, tomato_class_folder)
 
 # Load the labels
 label_path = "./model/labels.txt"
@@ -26,28 +24,35 @@ output_details = interpreter.get_output_details()
 _, height, width, _ = interpreter.get_input_details()[0]["shape"]
 print("Image Shape (", width, ",", height, ")")
 
-# List a random file for the model to predict
-file_list = os.listdir(full_path)
-random_image = random.choice(file_list)
 
-image = Image.open(full_path + random_image).convert("RGB").resize((width, height))
-image = np.array(image)
-processed_image = np.expand_dims(image, axis=0).astype(np.float32)
+def predict_new_disease():
+    # List a random file for the model to predict
+    tomato_class_folder = str(np.random.choice(os.listdir(images_folder)) + "/")
+    full_path = os.path.join(images_folder, tomato_class_folder)
+    file_list = os.listdir(full_path)
+    random_image = random.choice(file_list)
 
-interpreter.set_tensor(input_details[0]["index"], processed_image)
+    # Load the image
+    image = Image.open(full_path + random_image).convert("RGB").resize((width, height))
+    image = np.array(image)
+    processed_image = np.expand_dims(image, axis=0).astype(np.float32)
 
-t1 = time.time()
-interpreter.invoke()
-t2 = time.time()
-time_taken = (t2 - t1) * 1000  # milliseconds
-print("time taken for Inference: ", str(time_taken), "ms")
+    interpreter.set_tensor(input_details[0]["index"], processed_image)
 
-# Obtain results
-prediction = interpreter.get_tensor(output_details[0]["index"])[0]
-prediction = np.argmax(prediction)
+    t1 = time.time()
+    interpreter.invoke()
+    t2 = time.time()
+    time_taken = (t2 - t1) * 1000  # milliseconds
+    print("time taken for Inference: ", str(time_taken), "ms")
+
+    # Obtain results
+    prediction = interpreter.get_tensor(output_details[0]["index"])[0]
+    prediction = np.argmax(prediction)
+
+    compare_output(prediction, label_path, tomato_class_folder, random_image)
 
 
-def compare_output(output_value, label_path):
+def compare_output(output_value, label_path, tomato_class_folder, random_image):
     # Read the contents of the text file
     with open(label_path, "r") as file:
         lines = file.readlines()
@@ -66,6 +71,3 @@ def compare_output(output_value, label_path):
         print(f"Identified by model as: {labels[output_value]}")
     else:
         print("Output value not found in the text file.")
-
-
-compare_output(prediction, label_path)
